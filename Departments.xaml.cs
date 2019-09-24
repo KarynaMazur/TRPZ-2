39e06c67e01d835012d7f4b233e83b39d3b8469f
Karyna Mazur <50112425+KarynaMazur@users.noreply.github.com>
2019-09-24 08:46:36 +0300

    second commit

---
+++ Departments.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ITCompany
+{
+    /// <summary>
+    /// Логика взаимодействия для Departments.xaml
+    /// </summary>
+    public partial class Departments : Window
+    {
+        public Departments()
+        {
+            InitializeComponent();
+        }
+    }
+}
